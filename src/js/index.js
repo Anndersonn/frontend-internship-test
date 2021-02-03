c005076f43aabfa41f4f423f2cda15bcd58ac14d
Anndersonn <slobodjanik1997@gmail.com>
2021-02-03 22:44:23 +0100

    first task - html code, scss, simple js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 /* Here goes your JS code */
+const popup = document.querySelector('.popup')
+const showPopup = document.querySelector('#show-popup-form')
+const closePopup = document.querySelector('.close-popup')
+
+showPopup.addEventListener('click', () => { 
+    popup.classList.add('show')
+})
+
+closePopup.addEventListener('click', () => { 
+    popup.classList.remove('show')
+})
