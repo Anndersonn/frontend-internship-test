5a667ce35c1d7a49f76b80bdcf3581b3fae15a5a
Anndersonn <slobodjanik1997@gmail.com>
2021-02-03 22:57:01 +0100

    validation and success text

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
-/* Here goes your JS code */
+// POPUP 
 const popup = document.querySelector('.popup')
 const showPopup = document.querySelector('#show-popup-form')
 const closePopup = document.querySelector('.close-popup')
+// FORMS 
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const form = document.querySelector('.form')
+// TEXT
+const showText = document.querySelector('.show-text')
 
 showPopup.addEventListener('click', () => { 
     popup.classList.add('show')
@@ -9,4 +15,20 @@ showPopup.addEventListener('click', () => {
 
 closePopup.addEventListener('click', () => { 
     popup.classList.remove('show')
+})
+
+
+form.addEventListener('submit', (e) => { 
+    e.preventDefault()
+    if(password.value.length <= 5) { 
+        alert('Password must be longer than 5 characters')
+    }
+    if(password.value.length >= 12) { 
+        alert('Password must be less than 5 characters')
+    }
+    setTimeout(() => { 
+        popup.classList.add('hide')
+        showText.classList.add('show')
+        showPopup.classList.add('hide')
+    }, 3000)
 })
